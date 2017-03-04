f0b8716f29c0461166b17751e13b1f42f562022f
nabilafirst <nabilafirsta@gmail.com>
2017-03-04 10:45:39 +0700

    Navigation Drawer Set First Shown

--- app/src/main/java/id/sch/smktelkom_mlg/learn/navigation4/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/navigation4/MainActivity.java
@@ -44,6 +44,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changePage(R.id.nav_home);
+        navigationView.setCheckedItem(R.id.nav_home);
+
     }
 
     @Override
@@ -82,15 +86,21 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        Fragment fragment = null;
+
         int id = item.getItemId();
+        changePage(id);
+        return true;
+
+    }
+
+    private void changePage(int id) {
+
+        Fragment fragment = null;
 
         if (id == R.id.nav_home) {
             fragment = new HomeFragment();
             setTitle("Home");
-        }
-
-        if (id == R.id.nav_camera) {
+        } else if (id == R.id.nav_camera) {
             fragment = new ImportFragment();
             setTitle("Import");
         } else if (id == R.id.nav_gallery) {
@@ -109,6 +119,6 @@ public class MainActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+
     }
 }
